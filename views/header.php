575f5cef6110ba9c4a7c2f03f3fb91f4617eed5a
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-12-05 18:44:49 +0700

    added header

---
+++ views/header.php
@@ -0,0 +1,32 @@
+<header class="flex-row h-25 w-100 gap-5 justify-content-center bg-white d-flex align-items-center p-2">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <div class="fs-2" style="font-weight: 900;">
+        <a href="/" class="text-decoration-none text-dark">Shop.co</a>
+    </div>
+    <div class=" flex-row gap-2 align-items-center">
+        <button type="button" class="fw-bold bg-white btn-light btn fw-medium">Shop</button>
+        <button type="button" class="fw-bold bg-white btn-light btn fw-medium">On Sale</button>
+        <button type="button" class="fw-bold bg-white btn-light btn fw-medium">New Arrivals</button>
+        <button type="button" class="fw-bold bg-white btn-light btn fw-medium">Brands</button>
+    </div>
+    <div class="flex-row gap-2 align-items-center">
+        <!-- search bar -->
+        <form>
+            <label class="d-flex flex-row gap-1 bg-body-secondary rounded-pill align-items-center px-2">
+                <span class="visually-hidden">Search</span>
+                <iconify-icon icon="mdi:magnify" color="black" width="30" height="30"></iconify-icon>
+                <input class="form-control bg-transparent border-0" type="search" placeholder="Search" aria-label="Search">
+            </label>
+        </form>
+    </div>
+    <div class="flex-row gap-2 align-items-center">
+        <button type="button" title="Cart" class="bg-white btn-light btn fw-medium">
+            <!-- cart icon button -->
+            <iconify-icon icon="mdi:cart" color="black" width="30" height="30"></iconify-icon>
+        </button>
+        <!-- account icon button -->
+        <button type="button" title="Account" class="bg-white btn-light btn fw-medium">
+            <iconify-icon icon="mdi:account" color="black" width="30" height="30"></iconify-icon>
+        </button>
+    </div>
+</header>
