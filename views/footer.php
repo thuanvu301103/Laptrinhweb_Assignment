eb09d3b715fed7ec8a6ac846db16f3d58a8b74e6
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-12-05 16:45:29 +0700

    Add files via upload

---
+++ views/footer.php
@@ -0,0 +1,74 @@
+<footer class="px-4 bg-dark text-white">
+        <link href="./css/main.css" rel="stylesheet">
+        <div class="row border-bottom border-3 py-5">
+            <div class="col-lg-3 mb-3">
+                <a class="navbar-brand fs-3 fw-bolder" href="#">SHOP.CO</a>
+                <div class="text-secondary py-3">
+                    We have clothes that suits your style and which you’re proud to wear. From women to men.
+                </div>
+                <div>
+                    <a class="link-underline link-underline-opacity-0" href="#">
+                        <img class="socialmedia" alt="twitter" src="./img/footer/tw.png">
+                    </a>
+                    <a class="link-underline link-underline-opacity-0" href="#">
+                        <img class="socialmedia" alt="facebook" src="./img/footer/fb.png">
+                    </a>
+                    <a class="link-underline link-underline-opacity-0" href="#">
+                        <img class="socialmedia" alt="github" src="./img/footer/gh.png">
+                    </a>
+                    <a class="link-underline link-underline-opacity-0" href="#">
+                        <img class="socialmedia" alt="instargram" src="./img/footer/in.png">
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-6 col-lg-2 offset-lg-1 mb-3" bis_skin_checked="1">
+                <h5 class="text-uppercase py-3">Company</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">About</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Features</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Works</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Career</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 mb-3" bis_skin_checked="1">
+                <h5 class="text-uppercase py-3">Help</h5>
+                <ul class="list-unstyled fs-6">
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Customer Support</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Delivery Details</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Terms & Conditions</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 mb-3" bis_skin_checked="1">
+                <h5 class="text-uppercase py-3">faq</h5>
+                <ul class="list-unstyled fs-6">
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Account</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Manage Deliveries</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Orders</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Payments</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 mb-3" bis_skin_checked="1">
+                <h5 class="text-uppercase py-3">Resources</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Free eBooks</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Tutorial</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">How to - Blog</a></li>
+                    <li class="mb-2"><a class="text-secondary link-underline link-underline-opacity-0" href="#">Youtube Playlist</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row py-5">
+            <div class="col-lg-6 text-secondary text-center text-lg-start">
+                Shop.co © 2000-2023, All Rights Reserved
+            </div>
+            <div class="col-lg-6 mb-3 text-center text-lg-end">
+                <img class="payment p-0" alt="payment" src="./img/payment/visa.png">
+                <img class="payment p-0" alt="payment" src="./img/payment/master.png">
+                <img class="payment p-0" alt="payment" src="./img/payment/paypal.png">
+                <img class="payment p-0" alt="payment" src="./img/payment/applepay.png">
+                <img class="payment p-0" alt="payment" src="./img/payment/googlepay.png">
+            </div>
+        </div>
+</footer>
