48d2b851ddd1ad8aa5d8a0325784a09d803b7ee0
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-12-05 20:48:27 +0700

    fix icons and search

--- views/header.php
+++ views/header.php
@@ -1,5 +1,6 @@
 <header class="flex-row h-25 w-100 gap-5 justify-content-center bg-white d-flex align-items-center p-2">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
   <div class="fs-2" style="font-weight: 900;">
     <a href="/" class="text-decoration-none text-dark">Shop.co</a>
   </div>
@@ -11,11 +12,11 @@
   </div>
   <div class="flex-row gap-2 align-items-center">
     <!-- search bar -->
-    <form>
+    <form class="m-0">
       <label class="d-flex flex-row gap-1 bg-body-secondary rounded-pill align-items-center px-2">
         <span class="visually-hidden">Search</span>
         <iconify-icon icon="mdi:magnify" color="black" width="30" height="30"></iconify-icon>
-        <input class="form-control bg-transparent border-0" type="search" placeholder="Search" aria-label="Search">
+        <input class="search-box form-control bg-transparent border-0" type="search" placeholder="Search" aria-label="Search">
       </label>
     </form>
   </div>
@@ -29,4 +30,29 @@
       <iconify-icon icon="mdi:account" color="black" width="30" height="30"></iconify-icon>
     </button>
   </div>
+  <script>
+    $(document).ready(function() {
+      $('.search-box input[type="text"]').on("keyup input", function() {
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if (inputVal.length) {
+          $.get("../controllers/search.controller.php", {
+            term: inputVal
+          }).done(function(data) {
+            // Display the returned data in browser
+            resultDropdown.html(data);
+          });
+        } else {
+          resultDropdown.empty();
+        }
+      });
+
+      // Set search input value on click of result item
+      $(document).on("click", ".result p", function() {
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+      });
+    });
+  </script>
 </header>
