1e845c6be2dfda121fdaf05d80d1ccd9d15a47e9
Dang12312 <80524787+Dang12312@users.noreply.github.com>
2023-12-06 11:11:16 +0700

    fix route in header

--- views/header.php
+++ views/header.php
@@ -1,3 +1,4 @@
+
 <header class="flex-row h-25 w-100 gap-5 justify-content-between bg-white d-flex align-items-center p-2">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
@@ -16,38 +17,47 @@
     </form>
   </div>
   <div class="flex-row gap-2 align-items-center flex-nowrap d-flex">
-    <a href="/index.php/cart" title="Cart" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
-      <!-- cart icon button -->
+    <!-- login icon button -->
+    <a href="/index.php/login" title="Login" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
+      <iconify-icon icon="mdi:login" color="black" width="30" height="30"></iconify-icon>
+    </a>
+     <!-- logout icon button -->
+    <a href="/controllers/logout.php" title="Logout" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
+      <iconify-icon icon="mdi:logout" color="black" width="30" height="30"></iconify-icon>
+    </a>
+    <!-- cart icon button -->
+    <a href="<?php if(isset($_SESSION['id'])) {echo "./cart?userid=".$_SESSION['id'];}?>" title="Cart" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
+ 
       <iconify-icon icon="mdi:cart" color="black" width="30" height="30"></iconify-icon>
     </a>
     <!-- account icon button -->
-    <a href="/index.php/profile" title="Account" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
+    <a href="<?php if(isset($_SESSION['id'])) {echo "./profile?userid=".$_SESSION['id'];} else { echo "./login";} ?>" title="Account" class="bg-white btn-light btn fw-medium d-flex justify-content-center align-items-center">
       <iconify-icon icon="mdi:account" color="black" width="30" height="30"></iconify-icon>
     </a>
   </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script>
-    $(document).ready(function() {
-      $('.search-box input[type="text"]').on("keyup input", function() {
-        /* Get input value on change */
-        var inputVal = $(this).val();
-        var resultDropdown = $(this).siblings(".result");
-        if (inputVal.length) {
-          $.get("../controllers/search.controller.php", {
-            term: inputVal
-          }).done(function(data) {
-            // Display the returned data in browser
-            resultDropdown.html(data);
-          });
-        } else {
-          resultDropdown.empty();
-        }
+  $(document).ready(function(){
+      $('.search-box input[type="text"]').on("keyup input", function(){
+          /* Get input value on change */
+          var inputVal = $(this).val();
+          var resultDropdown = $(this).siblings(".result");
+          if(inputVal.length){
+              $.get("../controllers/search.controller.php", {term: inputVal}).done(function(data){
+                  // Display the returned data in browser
+                  resultDropdown.html(data);
+              });
+          } else{
+              resultDropdown.empty();
+          }
       });
-
+      
       // Set search input value on click of result item
-      $(document).on("click", ".result p", function() {
-        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
-        $(this).parent(".result").empty();
+      $(document).on("click", ".result p", function(){
+          $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+          $(this).parent(".result").empty();
       });
-    });
+  });
   </script>
 </header>
+  
